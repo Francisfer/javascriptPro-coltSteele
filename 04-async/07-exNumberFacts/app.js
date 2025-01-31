1bc287e432967ef7a5c3bde3e970b1f27b5d7736
Francisfer <114183517+Francisfer@users.noreply.github.com>
2025-01-31 09:59:34 +0000

    Javascript latest features.

--- 04-async/07-exNumberFacts/app.js
+++ 04-async/07-exNumberFacts/app.js
@@ -7,11 +7,10 @@ async function showNumberTrivia(number) {
   const response = await fetch(`http://numbersapi.com/${number}?json`);
   console.log(response);
   const data = await response.json();
-
-  console.log(data.text);
+  return data.text;
 }
 
-showNumberTrivia(27);
+// showNumberTrivia(27);
 
 // 2.
 
@@ -27,10 +26,10 @@ async function showNumberRace(...numbers) {
 
   const response = await Promise.race(promisesArray);
   const winner = await response.json();
-  console.log(winner.text);
+  return winner.text;
 }
 
-showNumberRace(2, 35, 6, 7);
+// showNumberRace(2, 35, 6, 7);
 
 // 3.
 
@@ -47,19 +46,40 @@ async function showNumberAll(...numbers) {
 
     const response = await Promise.allSettled(promisesArray);
 
-    const fulfilled = await Promise.allSettled(
+    const fulfilledData = await Promise.allSettled(
       response
         .filter((res) => res.value.ok === true)
-        .map((res) => console.log(res))
+        .map((data) => data.value.json())
     );
+    const fulfilled = fulfilledData.map((data) => data.value.text);
 
-    const rejected = response.filter((res) => res.value.ok === false);
+    const rejected = response
+      .filter((res) => res.value.ok === false)
+      .map((data) => `Request failed with status code: ${data.value.status}`);
 
-    console.log("fulfilled:", fulfilled);
-    console.log("rejected:", rejected);
+    return [fulfilled, rejected];
   } catch (error) {
     console.warn(error);
   }
 }
 
-showNumberAll(4, 2, "a", 7);
+// showNumberAll(4, 2, "a", 7);
+
+// 4
+
+async function main() {
+  const showTrivia = await showNumberTrivia(5);
+  console.log(`showNumberTrivia:`, showTrivia);
+  const showRace = await showNumberRace(42, 11, 66, 100);
+  console.log(`showNumberRace:`, showRace);
+  const [showAllFulfilled, showAllRejected] = await showNumberAll(
+    "WRONG",
+    25,
+    66,
+    245
+  );
+  console.log(`showNumberAll fulfilled:`, showAllFulfilled);
+  console.log(`showNumberAll rejected:`, showAllRejected);
+}
+
+main();
