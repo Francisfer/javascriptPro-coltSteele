75736eb1bc0aa0cb507ec6030e5138d63fba7fbb
Francisfer <114183517+Francisfer@users.noreply.github.com>
2025-02-03 14:48:04 +0000

    Timers: debouncing, throttling and request animation frame.

--- 07-timers/app.js
+++ 07-timers/app.js
@@ -7,16 +7,3 @@
 --> 
 
 */
-function solution(string) {
-  const index = [...string].filter((char) => char === char.toUpperCase());
-  const array = Array.from(string);
-
-  const word = [...string].slice(0, index);
-  const word1 = [...string].slice(index);
-  console.log(index);
-
-  // return "";
-}
-solution("camelCasingTest");
-
-const string = "camelCasingTest";
