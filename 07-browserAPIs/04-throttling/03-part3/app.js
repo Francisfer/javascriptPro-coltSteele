b6f8efcd4d51128864e35265fa5086815c7f469f
Francisfer <114183517+Francisfer@users.noreply.github.com>
2025-02-04 11:37:30 +0000

    Timers: debouncing, throttling and request animation frame.

--- 07-browserAPIs/04-throttling/03-part3/app.js
+++ 07-browserAPIs/04-throttling/03-part3/app.js
@@ -4,19 +4,11 @@
 
 /* 
 
---> So, let's implement some throttling.
+--> Now we will go through a more robust implementation of a throttle function, an actual reusable throttle function like we did with debounce.
 
-  -> A throttled function will allow loadMoreItems() to proceed, but then prevent further calls for some amount of time.
+  -> It is not advisable that we write this on our own, it is a simplified version of what other libraries throttle functions do. So it is not quite foolproof.
 
-  -> This allows us to control the rate (throttle the rate) that this function proceeds at (the rate that this function executes at).
-
-  -> We want to load data one time, give the user time to make he's wat through all the content, read it, understand it and then load more when he gets to the bottom.
-
-  -> One of the solutions is to use a library to implement debouncing like lodash.  
-
-  -> But here we do a very basic implementation of throttle to understand how it works.
-
-  -> In the next part we will set a more robust implementation.
+  -> So, for this, it is advisable to use a library or, at least, copy the code from a library if we need a throttle function.
 */
 
 function getRandomColor() {
@@ -58,44 +50,55 @@ function loadMoreItems() {
   }
 }
 
-// function throttle(callback, delay = 500) {
-//   let isThrottled = false;
-//   let savedArgs = null;
+// This function expects a callback function to be passed in, which will be throttle.
 
-//   const executeCallback = () => {
-//     if (savedArgs === null) {
-//       isThrottled = false;
-//     } else {
-//       callback(...savedArgs);
-//       savedArgs = null;
-//       setTimeout(executeCallback, delay);
-//     }
-//   };
+// Also, it expects a delay with a default of 500 milliseconds.
 
-//   return (...args) => {
-//     if (isThrottled) {
-//       savedArgs = args;
-//       return;
-//     }
+function throttle(callback, delay = 500) {
+  // this is the flag that indicates whether we should currently be delaying/throttling the function calls or not.
 
-//     callback(...args);
-//     isThrottled = true;
-//     setTimeout(executeCallback, delay);
-//   };
-// }
+  // It is set to false because we want our function to run initially.
+  let isThrottled = false;
 
-// const throttledLoadItems = throttle(loadMoreItems, 300);
+  // This variable will store any arguments that passed to our throttle function while we are in the middle of a throttle delay.
+  let savedArgs = null;
 
-let isThrottled = false;
+  // This is a helper function, it is responsible for managing our throttle delay and executing the callback function.
+
+  // If savedArgs is null, it means that no function calls where made during our delay period. So we can safely set isThrottled back to false.
+
+  // If it is not null, it means that we've received function calls during our delay period.
+
+  // At that point, we will execute the callback with the saved arguments.
+
+  // After this we set the saved args to null and, after the delay, we recursively call execute callback again and the same flow occurs.
+  const executeCallback = () => {
+    if (savedArgs === null) {
+      isThrottled = false;
+    } else {
+      callback(...savedArgs);
+      savedArgs = null;
+      setTimeout(executeCallback, delay);
+    }
+  };
+
+  // Here is where we return a new function that will be the returned value of calling throttle
+  return (...args) => {
+    if (isThrottled) {
+      savedArgs = args;
+      return;
+    }
+
+    callback(...args);
+    isThrottled = true;
+    setTimeout(executeCallback, delay);
+  };
+}
+
+const throttledLoadItems = throttle(loadMoreItems, 300);
 
 window.addEventListener("scroll", () => {
-  if (!isThrottled) {
-    loadMoreItems();
-    isThrottled = true;
-    setTimeout(() => {
-      isThrottled = false;
-    }, 300);
-  }
+  throttledLoadItems();
 });
 
 // Initial load
