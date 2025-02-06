c9c14a8b943b15d816fd5f502959902922f64eb0
Francisfer <114183517+Francisfer@users.noreply.github.com>
2025-02-06 18:58:09 +0000

    Functional Programming.

--- 08-funcitonalProgramming/10-diceGame/02-dgameSimpleFP/script.js
+++ 08-funcitonalProgramming/10-diceGame/02-dgameSimpleFP/script.js
@@ -10,21 +10,24 @@
 const getRandomRoll = () => Math.floor(Math.random() * 6 + 1);
 const checkWin = (roll) => roll === 6;
 
-const rollDiceAndDisplayResult = (resultDisplay) => {
+const displayResult = (element, message) => {
+  element.innerText = message;
+};
+
+const rollDiceAndDisplayResult = (resultDisplay) => () => {
   const roll = getRandomRoll();
-  console.log(resultDisplay);
-  resultDisplay.textContent = `You rolled a ${roll}! ${
-    roll === 6 ? "You Win!!!" : "Keep trying!"
-  }`;
+
+  const message = checkWin(roll)
+    ? `You rolled a ${roll}! You Win!!!`
+    : `You rolled a ${roll}! Keep Trying!`;
+
+  displayResult(resultDisplay, message);
 };
 
 const createDiceGame = (rollBtnId, resultDisplayId) => {
   const rollBtn = document.getElementById(rollBtnId);
   const resultDisplay = document.getElementById(resultDisplayId);
-  rollBtn.addEventListener(
-    "click",
-    rollDiceAndDisplayResult.bind(null, resultDisplay)
-  );
+  rollBtn.addEventListener("click", rollDiceAndDisplayResult(resultDisplay));
 };
 
 createDiceGame("rollBtn", "result");
